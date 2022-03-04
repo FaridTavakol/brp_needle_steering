2834bcd35000b748dc195f52052f6f1b3a524ef8
FaridTavakol <ftavakkolmoghadd@wpi.edu>
2022-03-04 13:51:59 -0500

    fixed bicycle model.

--- Test_script.m
+++ Test_script.m
@@ -16,11 +16,11 @@ e3 = [0;0;1];
 
 % Geometric Relations
 global l1;
-l1 = 20;      %mm
+l1 = 40;      %mm
 global l2;
-l2 = 5.633;  %mm
+l2 = 23.77;  %mm For unicycle model this will be zero
 global curvature;
-curvature = 0.1938;
+curvature = 0.00449;
 
 global V1;
 V1 = [e3; curvature * e1];
@@ -32,14 +32,14 @@ global T;
 T = 0.005;
 
 % Simulation configuration
-simulationTime = 40;       %second
+simulationTime = 10;       %second
 time = 0:T:simulationTime;  % simulation run time
 numberOfIterations = simulationTime/T + 1;
 
 
 
 % Initial conditions:
-insertionSpeed = 3;      % mm/s
+insertionSpeed = 23.50;      % mm/s
 % rotationSpeed = 32*pi;     % rad/s
 needleTipPos = zeros(3,1,numberOfIterations);
 needleTipPos(:,:,1) = [0;0;0];
@@ -57,17 +57,19 @@ FramePos = zeros(3, 1, numberOfIterations);
 FramePos(1:3,1) =  Gab(1:3,4,1)';
 
 % Configuration
-% w_max =deg2rad(100);   % Max rotation speed of needle in deg/sec
-w_max =100;   % Max rotation speed of needle in deg/sec
+
+w_max =deg2rad(100);   % Max rotation speed of needle in deg/sec
 c = 20;         % Need to optimize the gaussian width, this is a tuning parameter
 
 
 theta = zeros(1,numberOfIterations);   % Initial steering direction angle
+theta(1) = 0.01;
 RotationSpeed = zeros(1,numberOfIterations);
+w_hat = zeros(1,numberOfIterations);
 TotalInsertionDistance = zeros(1,numberOfIterations);
 
 % Command inputs, would come from higher level java control loop
-theta_d =180;   % Desired steering direction (0 - 360) (0 is in y-z plane)
+theta_d = pi;   % Desired steering direction (0 - 360) (0 is in y-z plane)
 alpha = 0.9;     % Steering effort (0 - 1), 0: straight, 1 curved
 % Could also add a rotation speed parameter to override w_max
 
@@ -75,38 +77,28 @@ for i = 1:numberOfIterations
     % Calculate the desired set point position 'theta(k+1)'
     % for the current servo loop interval
     %% NEED TO TAKE INTO ACCOUNT 0/360 DEGREE CROSSING
-    % control input inside control loop to generate complex curve
-    %     if k<0.5*(size(time,2)-1)
-    %       theta_d =180;
-    %       alpha = 0;
-    %     else
-    %       theta_d =180;
-    %       alpha = 0.8;
-    %     end
-    
-    %CHECK
-    if((theta(i)-theta_d)>180)
-        w_hat = 1 - alpha * exp(-(360-(theta(i)-theta_d))^2 / (2*c^2));
-    else
-        w_hat = 1 - alpha * exp(-(theta(i)-theta_d)^2 / (2*c^2));
-    end
+        
+%     w_hat(i) = 1 - alpha * exp(-(theta(i)-theta_d)^2 / (2*c^2));
+%     w_hat(i) = sin(theta(i)- 0.5*pi)/2 + 0.7;%1 - alpha * exp(-(theta(i)-theta_d)^2 / (2*c^2));
+    w_hat(i) = 0;
     
     
     % w = w_max * w^
-    RotationSpeed(i) = w_hat * w_max;
+    RotationSpeed(i) = w_hat(i) * w_max;
     
     % Determine angle for the next iteration
+%     theta(i+1) = theta(i) + RotationSpeed(i) * T;
+%     if(theta(i+1) > 2*pi)
+%         theta(i+1) = theta(i+1) - (2*pi);
+%     end
     theta(i+1) = theta(i) + RotationSpeed(i) * T;
-    if(theta(i+1)>360)
-        theta(i+1) = theta(i+1) - 360;
-    end
     
     % Change in the insertion
     u1 = insertionSpeed * T;
     TotalInsertionDistance(i+1) = TotalInsertionDistance(i) + u1;
     % Change in the angle
     u2 = RotationSpeed(i) * T;
-    % Calculating the kinematics using nonholonomic bicycle model   
+    % Calculating the kinematics using nonholonomic bicycle model
     [Gab(:,:,i+1), needleTipPos(:,:,i+1)] = bicycleKinematicsModelOneIteration(Gab(:,:,i), u1, u2, T);
     FramePos(:,:,i+1) = Gab(1:3,4,i+1);
     
@@ -119,13 +111,13 @@ plot(time,theta(1:end-1),'r','Linewidth',2);
 grid on
 title('Needle Rotation Angle Setpoints')
 xlabel('Time (sec)')
-ylabel('Rotation Angle')
+ylabel('Rotation Angle (rad)')
 subplot(2,1,2)
 plot(time,RotationSpeed,'b','Linewidth',2);
 grid on
 title('Needle Rotation Speed')
 xlabel('Time (sec)')
-ylabel('Rotation Speed')
+ylabel('Rotation Speed (rad/sec)')
 
 %Plot Trajectory
 figure(2);
@@ -133,12 +125,12 @@ hold on;
 plot3(needleTipPos(1,1:end),needleTipPos(2,1:end), needleTipPos(3,1:end), 'r','Linewidth',2);
 plot3(FramePos(1,:), FramePos(2,:), FramePos(3,:), 'b','Linewidth',2);
 grid on
-xlabel('x(cm)')
-ylabel('y(cm)')
-zlabel('z(cm)')
+xlabel('x(mm)')
+ylabel('y(mm)')
+zlabel('z(mm)')
 title('simulated trajectory')
 legend('tip','frame')
-axis equal
+% axis equal
 
 %Plot Trajectory
 figure(6);
@@ -146,8 +138,8 @@ hold on;
 plot(needleTipPos(3,:),needleTipPos(2,:), 'r','Linewidth',2);
 plot(FramePos(3,:), FramePos(2,:), 'b','Linewidth',2);
 grid on
-xlabel('z(cm)')
-ylabel('y(cm)')
+xlabel('z(mm)')
+ylabel('y(mm)')
 title('simulated trajectory')
 % legend('tip','frame')
 % axis equal
@@ -159,16 +151,16 @@ subplot(2,1,1)
 hold on;
 plot(FramePos(3,:), FramePos(2,:), 'b','Linewidth',2);
 grid on
-ylabel('y(cm)')
-xlabel('z(cm)')
+ylabel('y(mm)')
+xlabel('z(mm)')
 title('simulated trajectory of frame vs time')
 % axis equal
 subplot(2,1,2)
 hold on;
 plot(FramePos(3,:), FramePos(1,:), 'b','Linewidth',2);
 grid on
-ylabel('x(cm)')
-xlabel('z(cm)')
+ylabel('x(mm)')
+xlabel('z(mm)')
 % axis equal
 
 %Plot Trajectory
@@ -178,5 +170,5 @@ plot(time, [needleTipPos(1,1:end-1); needleTipPos(2,1:end-1)]);
 grid on
 title('simulated trajectory of tip vs time')
 xlabel('Time(sec)')
-ylabel('position(cm)')
+ylabel('position(mm)')
 legend('x','y')
--- bicycleKinematicsModelOneIteration.m
+++ bicycleKinematicsModelOneIteration.m
@@ -16,7 +16,6 @@ u1 = DeltaInsertionDistance;
 u2 = DeltaTheta*pi/180; %convert to rad
 
 %Solve kinematics for this iteration
-gab = gab0 * expSE3((u1 * toLieSE3(V1) + u2 * toLieSE3(V2)) * T);
-% gab = gab0 * expSE3((u1 * toLieSE3(V1) + u2 * toLieSE3(V2)));
+gab = gab0 * expSE3((u1 * toLieSE3(V1) + u2 * toLieSE3(V2)));
 nb = (gab(1:3,1:3) * l2 * e3) + gab(1:3,4);
 
